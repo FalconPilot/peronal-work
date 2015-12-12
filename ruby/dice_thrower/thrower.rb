54c30cf02d3b56eccdee716d85c16e9304acfbf1
FalconPilot <fpilot.github@gmail.com>
2015-12-12 02:00:03 +0100

    complexified program

--- ruby/dice_thrower/thrower.rb
+++ ruby/dice_thrower/thrower.rb
@@ -3,22 +3,41 @@
 # =======================================
 
 begin
-	puts "Generating a new random throw"
+	def prompt(*args)
+		print(*args)
+		value = gets.to_i
+		if value <= 0
+			puts "Invalid input ! (must be strictly positive number)"
+			exit
+		end
+		return value
+	end
 
-	tmin = 16
-	tmax = 20
-	stats = 6
-	dice = Array.new(stats, 0)
+	puts "======================="
+	puts "| Random dice-thrower |"
+	puts "======================="
 
+	throws = prompt("Enter dice count : ")
+	sides = prompt("Enter dice sides : ")
+	deriv = prompt("Enter dice range : ")
+
+	dice = Array.new(throws, 0)
+	tmin = (throws * sides) / 2 - deriv
+	tmax = (throws * sides) / 2 + deriv
+
+	puts "[Total should be between " + tmin.to_s + " and " + tmax.to_s + "]"
 	begin 
 		total = 0
-		stats.times do |index|
-			dice[index] = rand(1..6)
+		throws.times do |index|
+			dice[index] = rand(1..sides)
 			total += dice[index]
 		end 
 	end until total >= tmin && total <= tmax
+	puts "=================="
 	dice.each_with_index do |value, index|
-		puts "•ROLL " + (index + 1).to_s + " = " + value.to_s
+		puts "•ROLL " + (index + 1).to_s + " =	" + value.to_s
 	end
-	puts "Total value = " + total.to_s
+	puts "=================="
+	puts "Total value =	" + total.to_s
+	puts "=================="
 end
