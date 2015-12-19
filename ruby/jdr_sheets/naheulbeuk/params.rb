09f05ee7cbe494268295ff9f8b6ceaf0d0be6169
FalconPilot <fpilot.github@gmail.com>
2015-12-19 08:15:57 +0100

    replaced indents with spaces

--- ruby/jdr_sheets/naheulbeuk/params.rb
+++ ruby/jdr_sheets/naheulbeuk/params.rb
@@ -8,24 +8,24 @@ class Params
 # |             Initialization               |
 # ============================================
 
-	def initialize(params)
-		@params = params
-	end
+    def initialize(params)
+        @params = params
+    end
 
 
 # ============================================
 # |            Parse parameters              |
 # ============================================
 
-	def parse_params
-		modif = []
-		@params.each do |param|
-			case param
-				when "-nc"
-					modif += ["remove_colors"]
-			end
-		end
-		modif
-	end
+    def parse_params
+        modif = []
+        @params.each do |param|
+            case param
+                when "-nc"
+                    modif += ["remove_colors"]
+            end
+        end
+        modif
+    end
 
 end
