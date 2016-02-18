38aabd327a06f6e5d8b8678ba4700b2f23423f7f
FalconPilot <fpilot.github@gmail.com>
2016-02-18 23:18:54 +0100

    initial commit for pendu.rb

---
+++ ruby/pendu/pendu.rb
@@ -0,0 +1,159 @@
+# /------------------------------------------\
+# |          Jeu du pendu en Ruby            |
+# \------------------------------------------/
+
+begin
+
+# ============================================
+# |         Get word list from txt           |
+# ============================================
+
+    def get_word_list
+        words = ""
+        File.open('mots.txt', 'r') do |file|
+            while line = file.gets
+                words += line
+            end
+        end
+        list = words.split
+    end
+
+# ============================================
+# |          Wait for user input             |
+# ============================================
+
+    def prompt(*args)
+        print(*args)
+        gets
+    end
+
+# ============================================
+# |            Display content               |
+# ============================================
+
+    def display_content
+        print "\n#{@lives} Lives left	["
+        @lives.times do
+            print "O"
+        end
+        puts "]"
+        print "\nLetters used : "
+        @used.each do |letter|
+            print("#{letter} | ")
+        end
+        print("\n")
+        puts word_define
+    end
+
+# ============================================
+# |         Define word to display           |
+# ============================================
+
+    def word_define
+        display = ""
+        @word.each_with_index do |letter, i|
+            if @state[i] == 1 || !letter.match(/^[[:alpha:]]$/)
+                display += letter.upcase + " "
+            elsif !letter.match(/^[[:alpha:]]$/)
+                display += letter + " "
+            else
+                display += "_ "
+            end
+        end
+        display
+    end
+
+# ============================================
+# |             Define victory               |
+# ============================================
+
+    def victory?
+        victory = 0
+        @state.each do |s|
+            victory += s
+        end
+        victory
+    end
+
+# ============================================
+# |              Main routine                |
+# ============================================
+
+    def routine
+        while @lives > 0
+            if victory? == @word.size
+                puts "\nVictory ! :3\n"
+                return
+            end
+            display_content
+            refresh(prompt("Chose the next letter : ")[0])
+        end
+        puts "\nYou lost ! :'(\n"
+        print "The word was "
+        @word.each do |letter|
+            print letter
+        end
+        puts " !"
+    end
+
+# ============================================
+# |            Refresh function              |
+# ============================================
+
+    def refresh(guess)
+        correct = 0
+        if !guess.match(/^[[:alpha:]]$/)
+            puts "Incorrect character !"
+            return
+        end
+        @used.each do |used|
+            if guess == used
+                puts "Letter already guessed !"
+                return
+            end
+        end
+        @word.each_with_index do |letter, i|
+            if guess.upcase == letter && @state[i] == 0
+                correct += 1
+                @state[i] = 1
+            end
+        end
+        if correct == 0
+            @lives -= 1
+        end
+        @used += [guess]
+    end
+
+# ============================================
+# |             Color variables              |
+# ============================================
+
+    def colors_initialize
+    end
+
+# ============================================
+# |             Initialization               |
+# ============================================
+
+    def data_initialize
+        @lives = 5
+        @list = get_word_list
+        @word = @list[rand(@list.size)].split(//)
+        @state = Array.new(@word.size, 0)
+        @word.each_with_index do |letter, i|
+            @word[i] = letter.upcase
+            if !letter.match(/^[[:alpha:]]$/)
+                @state[i] = 1
+            end
+        end
+        @used = []
+    end
+
+# ============================================
+# |              Main program                |
+# ============================================
+
+    data_initialize
+    routine
+
+end
